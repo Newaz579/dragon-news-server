4deeddc30111f89886f21551965e20963b582d51
Newaz579 <newazsharif329@gmail.com>
2023-03-29 23:30:51 +0600

    server data commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const categories = require('./data/Categories/Categories.json');
+
+
+app.get('/', (req, res) => {
+    res.send('News Api updated');
+});
+
+app.get('/news-categories', (req, res) => {
+    res.send(categories);
+})
+app.listen(port, () => {
+    console.log('Dragon News Server running on port', port);
+})
