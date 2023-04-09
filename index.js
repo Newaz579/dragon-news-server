469949ef872d53942dfe848f0382d57e83a05729
Newaz579 <newazsharif329@gmail.com>
2023-04-09 06:06:22 +0600

    server side deploy final

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
+const cors = require('cors'); //when we are access to use data from another website
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 
 const categories = require('./data/Categories/Categories.json');
-
+const news = require('./data/News/News.json');
 
 app.get('/', (req, res) => {
     res.send('News Api updated');
@@ -14,7 +14,29 @@ app.get('/', (req, res) => {
 
 app.get('/news-categories', (req, res) => {
     res.send(categories);
+});
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news);
+    }
+    else {
+        const category_news = news.filter(n => n.category_id === id);
+        res.send(category_news);
+    }
 })
+
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
+})
+
 app.listen(port, () => {
     console.log('Dragon News Server running on port', port);
-})
+});
